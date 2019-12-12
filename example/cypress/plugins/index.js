fcac32966a5895a9ff588192699f01c40330216b
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2019-12-12 23:31:07 +0300

    feat(plugin): allow the users to configure browser args (#14)
    BREAKING CHANGE: `install` function has been split in two parts: `install` and `ensureRequiredBrowserFlags`.
    
    Before:
    
    ```js
    const { install } = require('@neuralegion/cypress-har-generator');
    
    module.exports = (on, config) => {
      install(on, config);
    };
    ```
    
    After:
    
    ```js
    const { install, ensureRequiredBrowserFlags } = require('@neuralegion/cypress-har-generator');
    
    module.exports = (on, config) => {
      install(on, config);
    
      on('before:browser:launch', (browser = {}, args) =>
        ensureRequiredBrowserFlags(browser, args)
      );
    };
    ```
    
    closes #13

--- example/cypress/plugins/index.js
+++ example/cypress/plugins/index.js
@@ -1,3 +1,9 @@
-const { install } = require('../../../dist');
+const { install, ensureRequiredBrowserFlags } = require('../../../dist');
 
-module.exports = (on, config) => install(on, config);
+module.exports = (on, config) => {
+  install(on, config);
+
+  on('before:browser:launch', (browser = {}, args) =>
+    ensureRequiredBrowserFlags(browser, args)
+  );
+};
