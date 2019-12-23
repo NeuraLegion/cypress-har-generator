d0753d12cd4e0b7dade716e5c8dd5bf8a0d2a430
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2019-12-23 21:42:51 +0300

    fix(commands): allow to pass custom filename

--- src/commands.ts
+++ src/commands.ts
@@ -1,18 +1,30 @@
 import { SaveOptions } from './index';
 
+const filename = (path: string): string | undefined => {
+  const startIndex: number =
+    path.indexOf('\\') >= 0 ? path.lastIndexOf('\\') : path.lastIndexOf('/');
+  let name: string = path.substring(startIndex);
+
+  if (name.indexOf('\\') === 0 || name.indexOf('/') === 0) {
+    name = name.substring(1);
+  }
+
+  const startExtIndex: number = name.lastIndexOf('.');
+
+  return name.substring(0, startExtIndex);
+};
+
+const harFileName = (path: string): string | undefined =>
+  `${filename(path)}.har`;
+
 Cypress.Commands.add('recordHar', () => cy.task('recordHar'));
 
-Cypress.Commands.add('saveHar', (fileName: string) => {
-  const specFileName: string | undefined = Cypress.spec?.name;
-
-  const defaultFileName: string = specFileName
-    ? `${specFileName.substring(0, specFileName.lastIndexOf('.'))}.har`
-    : 'archive.har';
-
+Cypress.Commands.add('saveHar', (fileName?: string) => {
+  const specFileName: string | undefined = Cypress.spec.name;
   const harsFolder: string = Cypress.env('hars_folders') ?? '.';
 
   cy.task('saveHar', {
     harsFolder,
-    fileName: fileName ?? defaultFileName
+    fileName: harFileName(fileName ?? specFileName)
   } as SaveOptions);
 });
