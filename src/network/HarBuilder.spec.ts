2b8f0057a9148769193a53c5a0d9311f6ec585c7
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2023-01-06 16:45:57 +0400

    test(network): cover HAR builder by unit tests (#123)

---
+++ src/network/HarBuilder.spec.ts
@@ -0,0 +1,70 @@
+import { HarBuilder } from './HarBuilder';
+import { Entry } from 'har-format';
+import { describe, beforeEach, it, expect } from '@jest/globals';
+
+const entries: Entry[] = [
+  {
+    startedDateTime: '2022-04-18T09:09:35.585Z',
+    time: -1,
+    request: {
+      method: 'GET',
+      url: 'https://example.com/',
+      httpVersion: 'HTTP/0.9',
+      headers: [],
+      queryString: [],
+      cookies: [],
+      headersSize: -1,
+      bodySize: -1
+    },
+    response: {
+      status: 200,
+      statusText: 'OK',
+      httpVersion: 'HTTP/0.9',
+      headers: [],
+      cookies: [],
+      content: {
+        size: -1,
+        mimeType: 'text/plain'
+      },
+      redirectURL: '',
+      headersSize: -1,
+      bodySize: -1
+    },
+    cache: {},
+    timings: { send: 0, receive: 0, wait: 0 }
+  }
+];
+
+describe('HarBuilder', () => {
+  let builder: HarBuilder;
+
+  beforeEach(() => {
+    builder = new HarBuilder(entries);
+  });
+
+  describe('build', () => {
+    it('should return an HAR', async () => {
+      // arrange
+      const {
+        name,
+        version,
+        homepage: comment
+      } = await import('../../package.json');
+      // act
+      const result = builder.build();
+      // assert
+      expect(result).toMatchObject({
+        log: {
+          entries,
+          version: '1.2',
+          pages: [],
+          creator: {
+            name,
+            version,
+            comment
+          }
+        }
+      });
+    });
+  });
+});
